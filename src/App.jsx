6b6d58e46c1606dd73e272f250ee8ba070583900
Rúbel Maneiro <rmaneiro28@gmail.com>
2025-01-18 19:34:11 -0400

    Add Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Home } from './components/Home'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <SpeedInsights />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Analytics>
+          <SpeedInsights />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Analytics>
       </BrowserRouter>
     </>
   )
